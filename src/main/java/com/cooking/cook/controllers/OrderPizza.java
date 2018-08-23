b366d4a83494dd3b66f1bf38f5de808cb4d014e5
jkloss <j.klos0405@gmail.com>
2018-08-23 19:21:59 +0200

    Added some pizza to menu

--- src/main/java/com/cooking/cook/controllers/OrderPizza.java
+++ src/main/java/com/cooking/cook/controllers/OrderPizza.java
@@ -2,9 +2,7 @@ package com.cooking.cook.controllers;
 
 import com.cooking.cook.data.Pizza;
 import com.cooking.cook.data.Restaurant;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,14 @@ public class OrderPizza {
     public void addPizza() {
        restaurant.getMenu().add(new Pizza(20.0, 40, "hawajska", true));
        restaurant.getMenu().add(new Pizza(24.0, 30, "kebab", true));
+       restaurant.getMenu().add(new Pizza(26.0, 40, "egrerg", true));
+       restaurant.getMenu().add(new Pizza(28.0, 30, "greegr", true));
+       restaurant.getMenu().add(new Pizza(24.0, 30, "bgfdb", true));
+       restaurant.getMenu().add(new Pizza(12.0, 30, "tyj", true));
+       restaurant.getMenu().add(new Pizza(30.0, 40, "qwdwq", true));
+       restaurant.getMenu().add(new Pizza(17.0, 30, "vsdd", true));
+       restaurant.getMenu().add(new Pizza(21.0, 30, "lui", true));
+       restaurant.getMenu().add(new Pizza(22.5, 50, "wev", true));
     }
 
     @GetMapping("/showMenu")
@@ -35,4 +41,7 @@ public class OrderPizza {
                 .filter(pizza -> pizza.getName().equals(name))
                 .collect(toList());
     }
+
+    @GetMapping("/sortNames")
+    public 
 }
