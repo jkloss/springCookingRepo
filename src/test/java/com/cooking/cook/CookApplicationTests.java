9574f1d85f23ecf6c9ccbd97e316e3f7c988613c
jkloss <j.klos0405@gmail.com>
2018-08-28 13:43:07 +0200

    Tests fixed

--- src/test/java/com/cooking/cook/CookApplicationTests.java
+++ src/test/java/com/cooking/cook/CookApplicationTests.java
@@ -1,16 +1,32 @@
 package com.cooking.cook;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class CookApplicationTests {
 
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup(WebApplicationContext webApplicationContext) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity()).build();
+    }
+
     @Test
     public void contextLoads() {
+
     }
 
 }
