fbd1bbc8687c634767121cd135876fdb3a856f4e
jkloss <j.klos0405@gmail.com>
2018-08-30 21:18:14 +0200

    Trying to find form solution

--- src/main/java/com/cooking/cook/controllers/PizzaController.java
+++ src/main/java/com/cooking/cook/controllers/PizzaController.java
@@ -1,35 +1,35 @@
 package com.cooking.cook.controllers;
 
+import com.cooking.cook.data.Pizza;
 import com.cooking.cook.service.PizzaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PizzaController {
 
     private PizzaService pizzaService;
-
     public PizzaController(PizzaService pizzaService) {
         this.pizzaService = pizzaService;
     }
 
-    @RequestMapping(value = "/addPizza", method = RequestMethod.GET)
-    public ModelAndView getForm() {
-        return new ModelAndView("addPizzaForm.html");
+//    @GetMapping("/showMenu")
+//    public List<Pizza> showMenu() {
+//        return pizzaService.getPizzaList();
+//    }
+
+    @RequestMapping(method = RequestMethod.GET, name = "/addPizza")
+    public String pizzaForm(ModelAndView model) {
+        model.addObject("pizza", new Pizza());
+        return "pizzaForm";
     }
 
-    @RequestMapping(value = "submitPizzaForm", method = RequestMethod.POST)
-    public ModelAndView submitPizzaForm(@RequestParam("pizzaName") String name,
-                                        @RequestParam("pizzaPrice") double price,
-                                        @RequestParam("pizzaDiameter") int diameter) {
-        ModelAndView model = new ModelAndView("submitPizzaForm.html");
-
-        model.addObject("msg", "Details: Name: " + name + "Price: " + price + "Diameter: "
-                + diameter);
-
-        return model;
+    @RequestMapping(method = RequestMethod.POST, value = "/addPizza")
+    public @ResponseBody String savePizza(Pizza pizza) {
+        return "hello" + pizza;
     }
+    
 }
