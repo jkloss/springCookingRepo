4ebd894de6932a7ac50d2fa4bb7a6cc6b3226de9
jkloss <j.klos0405@gmail.com>
2018-09-08 19:51:36 +0200

    Another test of view

--- src/main/java/com/cooking/cook/controllers/PizzaController.java
+++ src/main/java/com/cooking/cook/controllers/PizzaController.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -27,7 +27,7 @@ public class PizzaController {
     }
 
     @PostMapping("/addPizza")
-    public String submit(@Valid @ModelAttribute("pizza") final Pizza pizza,
+    public String submit(@Valid @RequestBody final Pizza pizza,
                          final BindingResult result, final ModelMap model) {
         if (result.hasErrors()) {
             return "error";
