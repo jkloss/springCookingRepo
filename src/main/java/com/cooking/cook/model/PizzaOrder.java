55005db2ddd503ff5b760325cd745f651ebc6fda
jkloss <j.klos0405@gmail.com>
2018-09-29 15:49:33 +0200

    Added createdBy filed in model for creating personalized views

--- src/main/java/com/cooking/cook/model/PizzaOrder.java
+++ src/main/java/com/cooking/cook/model/PizzaOrder.java
@@ -1,6 +1,7 @@
 package com.cooking.cook.model;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -24,11 +25,14 @@ public class PizzaOrder {
     @Min(value = 1)
     @NotNull
     private Integer amount;
+    @CreatedBy
+    private String orderCreatedBy;
 
 
-    public PizzaOrder(String orderName, Integer amount) {
+    public PizzaOrder(String orderName, Integer amount, String orderCreatedBy) {
         this.orderName = orderName;
         this.amount = amount;
+        this.orderCreatedBy = orderCreatedBy;
     }
 
     public PizzaOrder() {
@@ -58,12 +62,21 @@ public class PizzaOrder {
         this.amount = amount;
     }
 
+    public String getOrderCreatedBy() {
+        return orderCreatedBy;
+    }
+
+    public void setOrderCreatedBy(String orderCreatedBy) {
+        this.orderCreatedBy = orderCreatedBy;
+    }
+
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("PizzaOrder{");
         sb.append("id=").append(id);
         sb.append(", orderName='").append(orderName).append('\'');
         sb.append(", amount=").append(amount);
+        sb.append(", orderCreatedBy='").append(orderCreatedBy).append('\'');
         sb.append('}');
         return sb.toString();
     }
@@ -72,14 +85,15 @@ public class PizzaOrder {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof PizzaOrder)) return false;
-        PizzaOrder pizzaOrder = (PizzaOrder) o;
-        return amount == pizzaOrder.amount &&
-                Objects.equals(id, pizzaOrder.id) &&
-                Objects.equals(orderName, pizzaOrder.orderName);
+        PizzaOrder that = (PizzaOrder) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(orderName, that.orderName) &&
+                Objects.equals(amount, that.amount) &&
+                Objects.equals(orderCreatedBy, that.orderCreatedBy);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, orderName, amount);
+        return Objects.hash(id, orderName, amount, orderCreatedBy);
     }
 }
