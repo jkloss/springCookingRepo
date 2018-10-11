f10871a525a4c29778e637d888036192db5dbe00
jkloss <j.klos0405@gmail.com>
2018-10-11 21:50:08 +0200

    Model updated

--- src/main/java/com/cooking/cook/model/Customer.java
+++ src/main/java/com/cooking/cook/model/Customer.java
@@ -6,6 +6,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.Objects;
 
 @Entity
 public class Customer {
@@ -20,4 +21,60 @@ public class Customer {
     @NotBlank
     private String secretCode;
 
+    public Customer() {
+    }
+
+    public Customer(String name, String secretCode) {
+        this.name = name;
+        this.secretCode = secretCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecretCode() {
+        return secretCode;
+    }
+
+    public void setSecretCode(String secretCode) {
+        this.secretCode = secretCode;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Customer{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", secretCode='").append(secretCode).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) &&
+                Objects.equals(name, customer.name) &&
+                Objects.equals(secretCode, customer.secretCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, secretCode);
+    }
 }
